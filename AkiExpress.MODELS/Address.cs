7809d1ac7c4ddff407c0a67360b14627bd765e3a
Henri Espana <henriespana14@live.com>
2019-12-29 18:59:43 -0500

    initial commit

---
+++ AkiExpress.MODELS/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AkiExpress.MODELS
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string streetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int zipCode { get; set; }
+    }
+}
---
+++ AkiExpress.MODELS/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AkiExpress.MODELS
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public List<Address> Addresses { get; set; }
+    }
+}
